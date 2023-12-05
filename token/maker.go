84b670d58d8b3ba9da67caa1e08890f85d4b56f0
atanda0x <atandanafiu21@gmail.com>
2023-12-05 13:11:35 -0800

    token interface

---
+++ token/maker.go
@@ -0,0 +1,12 @@
+package token
+
+import "time"
+
+// Maker is an interface for managing token
+type Maker interface {
+	// CreateToken create a new token for a specific username, and duration
+	createToken(username string, duration time.Duration) (string, error)
+
+	// VerifyToken checks if the token is valid or not
+	VerifyToken(token string) (*payload, error)
+}
