1ef56a801f2ff04b4b0bfafd61f7974b7a436795
atanda0x <atandanafiu21@gmail.com>
2023-12-05 23:42:17 -0800

    added paseto-maker func

---
+++ token/paseto_maker.go
@@ -0,0 +1,52 @@
+package token
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/aead/chacha20poly1305"
+	"github.com/o1egl/paseto"
+)
+
+type PasetoMaker struct {
+	paseto      *paseto.V2
+	symmericKey []byte
+}
+
+func NewPasetoMaker(symmericKey string) (Maker, error) {
+	if len(symmericKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("invalid key size: must be exactly %d characters", chacha20poly1305.KeySize)
+	}
+
+	maker := &PasetoMaker{
+		paseto:      paseto.NewV2(),
+		symmericKey: []byte(symmericKey),
+	}
+	return maker, nil
+}
+
+// CreateToken create a new token for a specific username and duration
+func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) {
+	payload, err := NewPayLoad(username, duration)
+	if err != nil {
+		return "", err
+	}
+	return maker.paseto.Encrypt(maker.symmericKey, payload, nil)
+}
+
+// VerifyToken checks if the token is vbalid or not
+func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+
+	err := maker.paseto.Decrypt(token, maker.symmericKey, payload, nil)
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
+	}
+
+	return payload, nil
+}
