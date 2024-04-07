ec55a5818bef3d31d4cce0fd4d5ede3e0957b3c0
atanda0x <atandanafiu21@gmail.com>
2024-04-07 07:59:24 -0700

    sqlc entry geneated

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:ethereum@localhost:5432/fintechAPI?sslmode=disable"
+)
+
+var testQueries *Queries
+
+// testmain entry to the db
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/atanda0x/FintechConnect
 
 go 1.22.0
+
+require github.com/lib/pq v1.10.9
+
+require github.com/stretchr/testify v1.9.0 // indirect
