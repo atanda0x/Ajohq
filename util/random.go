ae1675197a7cd88d386446ee9c4c9993547432f9
atanda0x <atandanafiu21@gmail.com>
2024-04-07 12:40:49 -0700

    randomly geneared code func

--- util/random.go
+++ util/random.go
@@ -2,11 +2,8 @@ package util
 
 import (
 	"math/rand"
-	"strings"
 	"time"
 )
-
-var alphabet = "abcdefghijklmnopqrstuvxyz"
 
 func init() {
 	rand.NewSource(time.Now().UnixNano())
@@ -17,22 +14,11 @@ func RandomInt(min, max int64) int64 {
 	return min + rand.Int63n(max-min+1)
 }
 
-// RandomString generate a random string of len n
-func RandomString(n int) string {
-	var sb strings.Builder
-	k := len(alphabet)
-
-	for i := 0; i < n; i++ {
-		c := alphabet[rand.Intn(k)]
-		sb.WriteByte(c)
-	}
-
-	return sb.String()
-}
-
 // RandomOwner generate a random name
 func RandomOwner() string {
-	return RandomString(5)
+	owners := []string{"atanda", "ismail", "sultan", "munirah", "omowunmi", "victor", "nasu", "mujeeb", "samod", "rasheed", "osho", "agenla", "adeleke"}
+	n := len(owners)
+	return owners[rand.Intn(n)]
 }
 
 // RandomMoney generate a random amount of money
