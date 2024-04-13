487e19ece3e112faf3c7ff4e6028ea45e2398500
atanda0x <atandanafiu21@gmail.com>
2024-04-13 15:03:50 -0700

    db lock with

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -89,6 +89,31 @@ func (s Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTx
 		}
 
 		// TODO: FROMACCOUNT AND TOACCOUNT TX
+		account1, err := q.GetAccount(ctx, arg.FromAccountID)
+		if err != nil {
+			return err
+		}
+
+		result.FromAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+			ID:      arg.FromAccountID,
+			Balance: account1.Balance - arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		account2, err := q.GetAccount(ctx, arg.ToAccountID)
+		if err != nil {
+			return err
+		}
+
+		result.ToAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+			ID:      arg.ToAccountID,
+			Balance: account2.Balance + arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
 
 		return nil
 	})
